797d260a3c1e5ef5e63d88e6d0de339f3a60c836
vijaycode12 <reddyvijay4043@gmail.com>
2026-04-23 12:00:58 +0530

    Updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -81,7 +81,7 @@ export const logIn = async(req,res,next)=>{
         res.cookie('token',token,{
             httpOnly:true,
             secure:true,
-            sameSite:'lax',
+            sameSite:'none',
             maxAge:1000*60*60*24,
         });
 
